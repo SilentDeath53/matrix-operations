e8ff95211816c23d86bcfe86b3ff19707b160977
SilentDeath53 <48599339+SilentDeath53@users.noreply.github.com>
2023-06-01 12:07:31 +0300

    Create main.m

---
+++ main.m
@@ -0,0 +1,22 @@
+% Define two matrices
+matrix1 = [1, 2, 3; 4, 5, 6; 7, 8, 9];
+matrix2 = [9, 8, 7; 6, 5, 4; 3, 2, 1];
+
+% Addition
+addition = matrix1 + matrix2;
+
+% Subtraction
+subtraction = matrix1 - matrix2;
+
+% Multiplication
+multiplication = matrix1 * matrix2;
+
+% Display the results
+disp("Addition:");
+disp(addition);
+
+disp("Subtraction:");
+disp(subtraction);
+
+disp("Multiplication:");
+disp(multiplication);
